394e2a3a92455f15105905db376787cdef971d47
pineman <joao.castropinheiro@gmail.com>
2016-07-20 17:50:37 +0100

    initial gamestart support

--- server/abra.js
+++ server/abra.js
@@ -29,7 +29,7 @@ function getFileName(url) {
 // socket.io
 const io = require('socket.io')(app);
 
-const ROOM_TIMEOUT = 30;
+const ROOM_TIMEOUT = 30 * 1000;
 const MAX_PER_ROOM = 5;
 
 var texts = require('./texts.js');
@@ -62,6 +62,11 @@ function findRoom() {
 	return;
 };
 
+// Emit gamestart to all players in a room
+function emitGameStart(room) {
+	io.to(room.id).emit("gamestart");
+};
+
 io.on('connection', function (socket) {
 	// New player, find him a room
 	socket.on('newplayer', function (data) {
@@ -69,20 +74,34 @@ io.on('connection', function (socket) {
 		var room = findRoom();
 
 		if (room) {
+			// Close the room as soon as possible.
 			if (room.players.length === MAX_PER_ROOM - 1) {
 				room.status = "closed";
 			}
+			// Inform the players in the room that a new player
+			// has entered the room.
+			io.to(room.id).emit("playerentered", newPlayer);
 		} else {
 			// No open room was found, create a new room whose
 			// id is the first player's socket.id.
 			// NOTE: socket.io already does this by default
 			// http://socket.io/docs/rooms-and-namespaces/
-			room = Room(socket.id);
+			room = new Room(socket.id);
 			rooms.push(room);
+			// Start the game regardless of how many players
+			// are in the room in ROOM_TIMEOUT miliseconds
+			setInterval(emitGameStart, ROOM_TIMEOUT, room);
 		}
 
 		socket.join(room);
+		// No need to inform the player of itself, emit before append
 		socket.emit("foundroom", room);
 		room.players.push(newPlayer);
+
+		// Start the game if we have enough players
+		if (room.players.length === MAX_PER_ROOM) {
+			emitGameStart(room);
+		}
 	});
+
 });
